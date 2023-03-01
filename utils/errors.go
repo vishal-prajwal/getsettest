b7fe45dc74c2e5d932cad0f01ac808cf6a6d972b
sahil kukkar <sahil.kukkar99@gmail.com>
2023-03-01 18:36:16 +0530

    error object added for grpc

---
+++ utils/errors.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"fmt"
+
+	"google.golang.org/grpc/codes"
+)
+
+type NewGrpcError struct {
+	Code codes.Code
+	Err error
+}
+
+
+func (r *NewGrpcError) Error() string {
+	return fmt.Sprintf("status %d: err %v", r.Code, r.Err)
+}
