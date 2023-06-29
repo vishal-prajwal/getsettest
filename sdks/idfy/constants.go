8eaf1d330d0e25f8380a6dcc2bc06907b5a77305
sahil kukkar <sahil.kukkar99@gmail.com>
2023-06-29 10:54:04 +0530

    idfy chagnes

--- sdks/idfy/constants.go
+++ sdks/idfy/constants.go
@@ -1,12 +1,13 @@
 package idfy
 
 const (
-	AADHAR_DOC_TYPE    = "/ind_aadhaar"
-	PAN_DOC_TYPE       = "/ind_pan"
-	DL_DOC_TYPE        = "/ind_driving_license"
-	VOTER_DOC_TYPE     = "/ind_voter_id"
-	PASSPORT_DOC_TYPE  = "/ind_passport"
+	AADHAR_DOC_TYPE    = "/v3/tasks/sync/extract/ind_aadhaar"
+	PAN_DOC_TYPE       = "/v3/tasks/sync/extract/ind_pan"
+	DL_DOC_TYPE        = "/v3/tasks/sync/extract/ind_driving_license"
+	VOTER_DOC_TYPE     = "/v3/tasks/sync/extract/ind_voter_id"
+	PASSPORT_DOC_TYPE  = "/v3/tasks/sync/extract/ind_passport"
 	FraudCheckAadhar   = "/v3/tasks/async/verify_with_source/aadhaar_lite"
 	GetTaskStatus      = "/v3/tasks"
 	HealthCheckTimeout = 5
+	TemperedImage = "/v3/tasks/sync/check_tampering/document"
 )
