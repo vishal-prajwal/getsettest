54f853721eac07bde01d463bae9592c8e4f18d8f
Pavan Yewale <pavan.yewale@jungleegames.com>
2024-07-24 17:39:15 +0530

    profile req changes

--- sdks/jwr/struct.go
+++ sdks/jwr/struct.go
@@ -22,17 +22,17 @@ type UserProfile struct {
 }
 
 type UpdateUserProfileRequest struct {
-	FirstName  *string `json:"firstName"`
-	MiddleName *string `json:"middleName"`
-	LastName   *string `json:"lastName"`
-	DOB        *int64  `json:"dateOfBirth"`
-	Pin        *string `json:"pin"`
-	Address    *string `json:"address"`
-	Address2   *string `json:"address2"`
-	City       *string `json:"city"`
-	State      *string `json:"state"`
-	Gender     *string `json:"gender"`
-	Updatedby  *string `json:"updatedBy"`
+	FirstName  *string `json:"firstName,omitempty"`
+	MiddleName *string `json:"middleName,omitempty"`
+	LastName   *string `json:"lastName,omitempty"`
+	DOB        *int64  `json:"dateOfBirth,omitempty"`
+	Pin        *string `json:"pin,omitempty"`
+	Address    *string `json:"address,omitempty"`
+	Address2   *string `json:"address2,omitempty"`
+	City       *string `json:"city,omitempty"`
+	State      *string `json:"state,omitempty"`
+	Gender     *string `json:"gender,omitempty"`
+	Updatedby  *string `json:"updatedBy,omitempty"`
 }
 
 type JWRSDKConfig struct {
