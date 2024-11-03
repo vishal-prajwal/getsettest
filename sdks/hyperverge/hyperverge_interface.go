c99d8bf3929c5851c411b115002ec4caa0102c03
Khush Khandelwal <khush.khandelwal@jungleegames.com>
2024-11-03 15:54:16 +0530

    Add FraudCheckPanV2 method to Hyperverge interface for NSDL PAN verification

--- sdks/hyperverge/hyperverge_interface.go
+++ sdks/hyperverge/hyperverge_interface.go
@@ -9,6 +9,7 @@ type Hyperverge interface {
 	ReadPassport(hypervergeRequest HypervergeRequest) (*PassportResponse, error)
 	ReadVotedID(hypervergeRequest HypervergeRequest) (*VoterIdResponse, error)
 	FraudCheckPan(fraudCheckPanRequest FraudCheckPanRequest, txnID string) (*FraudCheckPanResponse, error)
+	FraudCheckPanV2(NSDLPanRequest NSDLPanRequest, txnID string) (*NSDLPanResponse, error)
 	FraudCheckDl(fraudCheckDlRequest FraudCheckDlRequest, txnID string) (*FraudCheckDlResponse, error)
 	FraudCheckVoter(fraudCheckVoterRequest FraudCheckVoterRequest, txnID string) (*FraudCheckVoterResponse, error)
 	FraudCheckPassport(fraudCheckPassportRequest FraudCheckPassportRequest, txnID string) (*FraudCheckPassportResponse, error)
