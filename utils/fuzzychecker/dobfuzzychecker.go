dee8ca42bea0212b0e234e5082b614195c46144e
Khush Khandelwal <khush.khandelwal@jungleegames.com>
2025-04-24 15:39:02 +0530

    refactor: Change FuzzyCompareDates return type to int for month difference

--- utils/fuzzychecker/dobfuzzychecker.go
+++ utils/fuzzychecker/dobfuzzychecker.go
@@ -5,20 +5,20 @@ import (
 	"time"
 )
 
-func FuzzyCompareDates(dateStr1, dateStr2 string) (bool, error) {
+func FuzzyCompareDates(dateStr1, dateStr2 string) (int, error) {
 	layout := "2006-01-02" // assuming same format for both
 
 	date1, err1 := time.Parse(layout, dateStr1)
 	date2, err2 := time.Parse(layout, dateStr2)
 	if err1 != nil || err2 != nil {
-		return false, fmt.Errorf("invalid date format")
+		return 0, fmt.Errorf("invalid date format")
 	}
 
 	y1, m1, _ := date1.Date()
 	y2, m2, _ := date2.Date()
 
 	if y1 != y2 {
-		return false, nil
+		return 0, nil
 	}
 
 	monthDiff := int(m1) - int(m2)
@@ -26,5 +26,5 @@ func FuzzyCompareDates(dateStr1, dateStr2 string) (bool, error) {
 		monthDiff = -monthDiff
 	}
 
-	return monthDiff <= 5, nil
+	return 1, nil
 }
